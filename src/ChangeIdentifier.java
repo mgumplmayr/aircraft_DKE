3109502317548269f7dc6b7cf68260602feb191f
Felix <k12043299@students.jku.at>
2023-01-09 21:02:42 +0100

    tests

--- src/ChangeIdentifier.java
+++ src/ChangeIdentifier.java
@@ -1,12 +1,12 @@
 import org.apache.jena.graph.Graph;
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.rdfconnection.RDFConnectionFuseki;
 import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.shacl.Shapes;
+import org.apache.jena.sparql.resultset.RDFOutput;
 import org.apache.jena.util.FileUtils;
 import org.topbraid.jenax.util.JenaUtil;
 import org.topbraid.shacl.rules.*;
@@ -23,6 +23,16 @@ public class ChangeIdentifier {
                 .destination("http://localhost:3030/aircraft/");
 
         Query query = QueryFactory.create("SELECT * { BIND('Hello'as ?text) }");
+        Query query1 = QueryFactory.create("PREFIX voc: <http://example.org/vocabulary#>\n" +
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "select\n" +
+                "  ?s ?p ?o\n" +
+                "    WHERE {\n" +
+                "  Graph ?g{\n" +
+                "  ?s ?p ?o.\n" +
+                "    ?s rdf:type voc:Position.\n" +
+                "  }\n" +
+                "}");
 
         // In this variation, a connection is built each time.
         try (RDFConnectionFuseki conn = (RDFConnectionFuseki) builder.build()) {
@@ -38,8 +48,29 @@ public class ChangeIdentifier {
              System.out.println(test);
              */
 
+            Model test = ModelFactory.createDefaultModel();
+            ResultSet rs = conn.query(query1).execSelect();
+
+            while (rs.hasNext()){
+                QuerySolution qs = rs.next();
+
+                Resource subject = qs.getResource("s");
+                Property predicate = test.createProperty(qs.get("p").toString());
+                RDFNode object = qs.get("o");
+
+                test.add(subject,predicate,object);
+            }
+
+            //test.add(RDFOutput.encodeAsModel(rs));
+            try {
+                test.write(new FileOutputStream("out/test.ttl"),"TTL");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            //System.out.println(test);
+
             Model fetchModel = conn.fetch("http://localhost:3030/aircraft/static/");
-            Graph ruleGraph = RDFDataMgr.loadGraph("shacl/ChangeIdentifierRules.ttl");
+            //Graph ruleGraph = RDFDataMgr.loadGraph("shacl/ChangeIdentifierRules.ttl");
             Model shapesModel= JenaUtil.createMemoryModel();
 
             try { //add rules to model
@@ -51,7 +82,7 @@ public class ChangeIdentifier {
 
             //infer Triples from rules
             Model result = RuleUtil.executeRules(fetchModel,shapesModel,null,null);
-
+            result.add(test);
             try { //write infered triples to file
                 result.write(new FileOutputStream("out/inference.ttl"),"TTL");
             } catch (FileNotFoundException e) {
