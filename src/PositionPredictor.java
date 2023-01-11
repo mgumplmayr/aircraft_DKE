8c0b93e6d3acdfe6300d549809608ab5eaf9b3a9
mgumplmayr <max0gum@yahoo.com>
2023-01-11 15:53:15 +0100

    almost done except upload

--- src/PositionPredictor.java
+++ src/PositionPredictor.java
@@ -1,10 +1,12 @@
-import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.*;
-import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdfconnection.RDFConnectionFuseki;
 import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
 import org.topbraid.jenax.util.JenaUtil;
-import org.topbraid.shacl.rules.*;
+import org.topbraid.shacl.rules.RuleUtil;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -69,52 +71,32 @@ public class PositionPredictor {
         );
 
         try (RDFConnectionFuseki conn = (RDFConnectionFuseki) builder.build()) {
-            /* Funktioniert
-             Model fetch = conn.fetch("http://localhost:3030/aircraft/static/");
-             System.out.println(fetch);*/
-
+            //getting Response from last 3 Graphs
             responseModel = conn.query(resultQuery).execConstruct();
-            QuerySolution q =conn.query(latestGraphQuery).execSelect().nextSolution();
-            String graph = q.get("g").toString();
-            System.out.println(graph);
 
-            try {
-                responseModel.write(new FileOutputStream("out/response.ttl"), "TTL");
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+            //getting the latest Graph for Upload
+            QuerySolution q = conn.query(latestGraphQuery).execSelect().nextSolution();
+            String graphURL = q.get("g").toString();
 
+            //add rules to model
             Model shapesModel = JenaUtil.createMemoryModel();
-
-            try { //add rules to model
-                shapesModel.read("shacl/PositionPredictorRules.ttl");
-                shapesModel.write(new FileOutputStream("out/testRules.ttl"), "TTL");
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
+            shapesModel.read("shacl/PositionPredictorRules.ttl");
 
             //infer Triples from rules
             Model result = RuleUtil.executeRules(responseModel, shapesModel, null, null);
-            //result.add(responseModel);
-            try { //write infered triples to file
-                result.write(new FileOutputStream("out/inference.ttl"), "TTL");
+            try {
+                //write infered triples to file
+                result.write(new FileOutputStream("out/result.ttl"), "TTL");
             } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
             }
             System.out.println("SHACL-Rule for Position Prediction executed");
 
-            /*String graphURL = connectionURL + "states/" + responseTime;
-            System.out.println("Uploading dynamic Graph data to endpoint " + graphURL);
-            log.append("Uploading dynamic Graph data to endpoint " + graphURL + "\n");
-
-            try (RDFConnection conn = RDFConnection.connect(connectionURL)) {
-                conn.put(graphURL, dynamicModel);
-            }
-            System.out.println("Upload of dynamic Graph data complete");
-            log.append("Upload of dynamic Graph data complete\n");*/
-
-
+            System.out.println("Uploading generated Position Prediction data to endpoint " + graphURL);
+            conn.put(graphURL, result);
+            System.out.println("Upload of Position Prediction data complete");
 
         }
     }
+
 }
